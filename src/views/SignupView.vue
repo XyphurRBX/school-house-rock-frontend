4743220f619475ec7ac05d5f9c6eaf8a780b6d85
Xyphur <xyphurrbx@gmail.com>
2022-07-24 05:14:18 -0500

    add alerts for signup

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,17 +1,46 @@
 <template>
 	<PreAuthHeader />
 	<AuthWindow color="primary" title="Sign Up" @submit="onSubmit" />
+
+	<v-alert
+		class="mx-4"
+		v-model="signupSuccess"
+		variant="tonal"
+		closable
+		type="success"
+		>Successfully signed up!</v-alert
+	>
+	<v-alert
+		class="mx-4"
+		v-model="signupFailure"
+		variant="tonal"
+		closable
+		type="error"
+		>Sign up failed.</v-alert
+	>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import PreAuthHeader from "../components/PreAuthHeader.vue";
 import AuthWindow from "../components/AuthWindow.vue";
 
 import { signup } from "../api-services/auth";
 
+const signupSuccess = ref(false);
+const signupFailure = ref(false);
+
 function onSubmit(username, password) {
-	signup(username, password).then((res) => {
-		console.log(res);
-	});
+	signup(username, password)
+		.then((res) => {
+			if (res.status == 200) {
+				signupSuccess.value = true;
+			} else {
+				signupFailure.value = true;
+			}
+		})
+		.catch(() => {
+			signupFailure.value = true;
+		});
 }
 </script>
