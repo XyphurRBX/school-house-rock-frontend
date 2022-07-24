24949671566c60275bcb2894bd27aed256f0c0b5
Xyphur <xyphurrbx@gmail.com>
2022-07-23 19:08:53 -0500

    allow all hosts

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
+    proxy: process.env.API_SERVER_URL,
+    allowedHosts: "all",
   },
 });
