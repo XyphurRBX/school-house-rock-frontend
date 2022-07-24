8451f8ca2787232aaaa143ac7b327846a7248a02
Xyphur <xyphurrbx@gmail.com>
2022-07-24 02:33:17 -0500

    get api endpoint

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Axios from "axios";
 
 loadFonts();
+
+Axios.defaults.baseURL = process.env.API_ENDPOINT;
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
