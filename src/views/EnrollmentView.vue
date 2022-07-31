19c24f97d2a1caad7575ec5e80fb8f510f478669
cdriver930 <cdriver27@gmail.com>
2022-07-31 15:45:34 -0500

    Added all views
    All pages now show their respective database table

--- src/views/EnrollmentView.vue
+++ src/views/EnrollmentView.vue
@@ -1,7 +1,9 @@
 <template>
 	<DashboardHeader title="Enrollment" />
+	<EntityEditor entityName="enrolled" />
 </template>
 
 <script setup>
 import DashboardHeader from "../components/DashboardHeader.vue";
+import EntityEditor from "../components/EntityEditor.vue";
 </script>
