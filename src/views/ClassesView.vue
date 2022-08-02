182a89f1f2223c6ceee24c6958c812a61761d9a5
hdaggie123 <hdemny@tamu.edu>
2022-08-02 10:49:27 -0500

    Update ClassesView.vue

--- src/views/ClassesView.vue
+++ src/views/ClassesView.vue
@@ -1,3 +1,6 @@
+VITE_API_ENDPOINT="https://school-house-rock-backend-kuym5.ondigitalocean.app"
+Access-Control-Allow-Origin: http://127.0.0.1:5173
+
 <template>
 	<DashboardHeader title="Classes" />
 	<EntityEditor entityName="class" />
